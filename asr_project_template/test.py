543bbf64f09b9f9a2be1c44b0690dc247a3aa425
Yan <Yan@Yan>
2023-10-26 19:57:09 +0300

    fix

--- asr_project_template/test.py
+++ asr_project_template/test.py
@@ -83,12 +83,9 @@ def main(config, out_file):
                     {
                         "ground_trurh": batch["text"][i],
                         "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
-                        "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["probs"][i], beam_size=1
-                        )[:10],
-                        "pred_text_lm_search": "NaN" if text_encoder.lm_use == False else\
-                            text_encoder.ctc_beam_search_with_lm(batch["probs"][i][None, :],
-                            torch.tensor([batch["log_probs_length"][i]]), 4)
+                        "pred_text_lm_search": text_encoder.ctc_beam_search_with_lm(batch["probs"][i][None, :], 
+                                                                                    torch.tensor([batch["log_probs_length"][i]]), 
+                                                                                    4)
                     }
                 )
     for metric_name in evaluation_metrics.keys():
