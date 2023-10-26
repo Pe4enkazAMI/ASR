65c2efb91c50178564ffb00c8d839b056968f2ba
Yan <Yan@Yan>
2023-10-26 18:17:56 +0300

    finally

--- asr_project_template/hw_asr/text_encoder/ctc_char_text_encoder.py
+++ asr_project_template/hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -4,8 +4,9 @@ import torch
 from collections import defaultdict
 from scipy.special import softmax
 from .char_text_encoder import CharTextEncoder
-
-
+from hw_asr.utils import ROOT_PATH
+from pyctcdecode import build_ctcdecoder
+import multiprocessing
 class Hypothesis(NamedTuple):
     text: str
     prob: float
@@ -14,11 +15,28 @@ class Hypothesis(NamedTuple):
 class CTCCharTextEncoder(CharTextEncoder):
     EMPTY_TOK = "^"
 
-    def __init__(self, alphabet: List[str] = None):
+    def __init__(self, alphabet: List[str] = None, lm = True):
         super().__init__(alphabet)
-        vocab = [self.EMPTY_TOK] + list(self.alphabet)
-        self.ind2char = dict(enumerate(vocab))
+        self.vocab = [self.EMPTY_TOK] + list(self.alphabet)
+        self.ind2char = dict(enumerate(self.vocab))
         self.char2ind = {v: k for k, v in self.ind2char.items()}
+
+        self.lm_use = lm
+        self.kenlm = ROOT_PATH / "data" / "lm" / "librispeech" / "3-gram.arpa"
+        if self.lm_use:
+            self.ldecoder = self._create_lm_decoder()
+
+
+    def _create_lm_decoder(self):
+
+        with open("/Users/bayesian_monster/DLA/asr_project_template/hw_asr/text_encoder/librispeech-vocab.txt") as f:
+            unigrams = [w.strip() for w in f.readlines()]
+            decoder = build_ctcdecoder(
+                [""] + [w.upper() for w in self.alphabet],
+                kenlm_model_path=str(self.kenlm),
+                unigrams=unigrams
+            )
+        return decoder
 
     def ctc_decode(self, inds: List[int]) -> str:
         last_char = self.EMPTY_TOK
@@ -55,9 +73,7 @@ class CTCCharTextEncoder(CharTextEncoder):
 # спиздил с сема
     def ctc_beam_search(self, probs: torch.tensor,
                         beam_size: int = 100) -> List[Hypothesis]:
-        """
-        Performs beam search and returns a list of pairs (hypothesis, hypothesis probability).
-        """
+
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
@@ -78,3 +94,18 @@ class CTCCharTextEncoder(CharTextEncoder):
         sorted_beam = sorted(final_beam.items(), key=lambda x: -x[1])
         result = [Hypothesis(text, v) for text, v in sorted_beam]
         return result
+    
+
+    def ctc_beam_search_with_lm(self, probs: torch.tensor, lengths: torch.tensor,
+            beam_size: int = 100) -> List[str]:
+
+        probs = torch.nn.functional.log_softmax(probs, -1)
+
+        logits_list = [probs[i][:lengths[i]].numpy() for i in range(lengths.shape[0])]
+
+        with multiprocessing.get_context("fork").Pool() as pool:
+            text_list = self.ldecoder.decode_batch(pool, logits_list, beam_width=beam_size)
+
+        text_list = [elem.lower().replace(self.EMPTY_TOK, "") for elem in text_list]
+
+        return text_list
