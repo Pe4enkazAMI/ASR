47e4f674c7cdc9b7fab62dc7ed30d8c6705242ca
Yan <Yan@Yan>
2023-10-26 14:42:29 +0300

    resume_checkpoint

--- asr_project_template/hw_asr/metric/cer_metric.py
+++ asr_project_template/hw_asr/metric/cer_metric.py
@@ -28,15 +28,26 @@ class ArgmaxCERMetric(BaseMetric):
         return sum(cers) / len(cers)
 
 class BeamSearchCER(BaseMetric):
-    def __init__(self, text_encoder: BaseTextEncoder, beam_size=3, *args, **kwargs):
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size=3, lm=True,  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.text_encoder = text_encoder
         self.beam_size = beam_size
+        self.lm = lm
+
     def __call__(self, log_probs, log_probs_length, text, **kwargs):
+        metric = []
+        if self.lm: 
+            log_probs = torch.nn.functional.log_softmax(log_probs.detach().cpu(), -1)
+            log_probs_length = log_probs_length.detach().cpu()
+            best_hypos = self.text_encoder.ctc_beam_search_with_lm(log_probs, log_probs_length,
+                                                                  self.beam_size)
+            for pred_text, target_text in zip(best_hypos, text):
+                    metric.append(calc_cer(target_text, pred_text))
+            return sum(metric) / len(metric)
+
+    
         preds = log_probs.detach().cpu().numpy()
         lens = log_probs_length.detach().numpy()
-        metric = []
-
         for probs, len_, target in zip(preds, lens, text):
             target = self.text_encoder.normalize_text(target)
             hypos = self.text_encoder.ctc_beam_search(probs[:len_], self.beam_size)
--- asr_project_template/hw_asr/metric/wer_metric.py
+++ asr_project_template/hw_asr/metric/wer_metric.py
@@ -27,16 +27,26 @@ class ArgmaxWERMetric(BaseMetric):
         return sum(wers) / len(wers)
     
 class BeamSearchWER(BaseMetric):
-    def __init__(self, text_encoder: BaseTextEncoder, beam_size=3, *args, **kwargs):
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size=3, lm=True,  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.text_encoder = text_encoder
         self.beam_size = beam_size
+        self.lm = lm
 
     def __call__(self, log_probs, log_probs_length, text, **kwargs):
+        metric = []
+        if self.lm: 
+            log_probs = torch.nn.functional.log_softmax(log_probs.detach().cpu(), -1)
+            log_probs_length = log_probs_length.detach().cpu()
+            best_hypos = self.text_encoder.ctc_beam_search_with_lm(log_probs, log_probs_length,
+                                                                  self.beam_size)
+            for pred_text, target_text in zip(best_hypos, text):
+                    metric.append(calc_wer(target_text, pred_text))
+            return sum(metric) / len(metric)
+
+    
         preds = log_probs.detach().cpu().numpy()
         lens = log_probs_length.detach().numpy()
-        metric = []
-
         for probs, len_, target in zip(preds, lens, text):
             target = self.text_encoder.normalize_text(target)
             hypos = self.text_encoder.ctc_beam_search(probs[:len_], self.beam_size)
